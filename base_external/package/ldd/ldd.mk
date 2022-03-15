814cf345390b45d396ee0b02c610682b44cbe18e
Sankalp Agrawal <43979892+sankalp123321@users.noreply.github.com>
2022-03-15 08:40:03 -0600

    chore: revert back the changes

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -14,8 +14,8 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-sankalp123321.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-#LDD_MODULE_SUBDIRS = scull/
-#LDD_MODULE_SUBDIRS += misc-modules/
+LDD_MODULE_SUBDIRS = scull/
+LDD_MODULE_SUBDIRS += misc-modules/
 
 
 
